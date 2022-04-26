a01a04aba727ac72a7c8b0478fde9114297a631b
JonathanAMichaels <jonathan.asher.michaels@gmail.com>
2022-04-26 16:03:11 -0400

    registration almost done

---
+++ __init__.py
@@ -0,0 +1,5 @@
+from . import geometries
+from . import registration
+from . import sorting
+from . import pipeline
+from . import pipeline_utils
--- registration/registration.py
+++ registration/registration.py
@@ -1,13 +1,13 @@
 import os
-import numpy as np
+import glob
 from ibllib.io.spikeglx import Reader
-import estimate_displacement as ed
-from utils import mat2npy
+import registration.estimate_displacement as ed
+from registration.utils import mat2npy
 
 
 def registration(config):
     # This implementation has been tested with Neuropixels 1.0
-    geomarray = mat2npy('../geometries/neuropixPhase3B1_kilosortChanMap.mat') # convert .mat chan file to .npy chan file
+    geomarray = mat2npy(config['script_dir'] + '/geometries/neuropixPhase3B1_kilosortChanMap.mat') # convert .mat chan file to .npy chan file
 
     # I've only tested the spikeglx data reader that's part of ibllib (pip install ibllib)
     # yass is the default reader, but I've removed any mandatory yass imports in case you don't have that
@@ -20,15 +20,18 @@ def registration(config):
     registration_type = config['Registration']['registration_type']
     horz_smooth = config['Registration']['horz_smooth']
 
+    folders = glob.glob(config['neuropixel'] + '/*_g*')
     for pixel in range(config['num_neuropixels']):
-        file_name = config['neuropixel'][0:-3]
-        working_directory = file_name + '_g0/' + file_name + '_g0_imec' + str(pixel) + '/'
+        working_directory = folders[pixel] + '/'
         registration_directory = working_directory + 'NeuropixelsRegistration/'
         if not os.path.exists(registration_directory):
             os.makedirs(registration_directory)
 
         # Prepare the data loader
-        reader = Reader(working_directory + file_name + '_g0_t0.imec' + str(pixel) +'.ap.bin')
+        file = glob.glob(working_directory + '*_t*.imec' + str(pixel) + '.ap.bin')
+        if len(file) != 1:
+            raise SystemExit('Invalid Neuropixel data: ' + str(file))
+        reader = Reader(file[0])
 
         if detect_spikes:
             # detect spikes
