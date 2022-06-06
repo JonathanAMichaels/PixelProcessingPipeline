edff236a30e20bd49e29cedccf91b27460f5005e
JonathanAMichaels <Jonathan.asher.michaels@gmail.com>
2022-06-06 12:06:44 -0400

    refactoring datashift

--- sorting/pykilosort/pykilosort/datashift2.py
+++ sorting/pykilosort/pykilosort/datashift2.py
@@ -630,10 +630,12 @@ def datashift2(ctx):
 
     # re-interpolate dispmap to match the number of batches
     batch_spacing = np.linspace(start=0, stop=disp_map.shape[0]-1, num=Nbatch)
+    new_disp_map = np.zeros((Nbatch, disp_map.shape[1]))
     for i in range(disp_map.shape[1]):
         new_disp_map[:, i] = np.interp(batch_spacing, np.arange(disp_map.shape[0]), disp_map[:, i])
     disp_map = new_disp_map
     print(disp_map.shape)
+    print(disp_map)
 
     ir.xc, ir.yc = probe.xc, probe.yc
 
