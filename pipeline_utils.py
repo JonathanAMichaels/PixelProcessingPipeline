3827e18bef17e40057ad8c3b74f15ea63f12b8f0
JonathanAMichaels <jonathan.asher.michaels@gmail.com>
2022-06-09 15:00:00 -0400

    putting back find

--- pipeline_utils.py
+++ pipeline_utils.py
@@ -7,6 +7,18 @@ from sorting.readSGLX import readMeta, SampRate, makeMemMapRaw, ExtractDigital
 import scipy.io
 
 
+def find(pattern, path):
+    result = []
+    for root, dirs, files in os.walk(path):
+        for name in files:
+            if fnmatch.fnmatch(name, pattern):
+                result.append(os.path.join(root, name))
+        for name in dirs:
+            if fnmatch.fnmatch(name, pattern):
+                result.append(os.path.join(root, name))
+    return result
+
+
 def create_config(script_folder, folder):
     shutil.copyfile(script_folder + '/config_template.yaml', folder + '/config.yaml')
 
