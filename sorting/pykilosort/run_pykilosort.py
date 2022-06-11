98633243f41e17e67f66631041b66022c28b6dea
JonathanAMichaels <Jonathan.asher.michaels@gmail.com>
2022-06-11 12:14:59 -0400

    drift test

--- sorting/pykilosort/run_pykilosort.py
+++ sorting/pykilosort/run_pykilosort.py
@@ -22,7 +22,12 @@ def kilosort(config):
     with h5py.File(registered_file[0], "r") as f:
         dispmap = f["dispmap"][:]
 
-    params['nblocks'] = np.round((dispmap.shape[0] + 1) / 2)
+    params['nblocks'] = 193
     params['disp_map'] = dispmap.tolist()
     run_spike_sorting_ibl(bin_file, delete=True, scratch_dir=scratch_dir,
                           ks_output_dir=ks_output_dir, log_level='INFO', params=params)
+
+    # correct params.py to point to the shifted data
+    with open(str(ks_output_dir) + '/params.py', 'w') as f:
+        f.write("dat_path = 'proc.dat'\nn_channels_dat = 384\ndtype = 'int16'\noffset = 0\n" +
+                "hp_filtered = True\nsample_rate = 30000\ntemplate_scaling = 20.0")
