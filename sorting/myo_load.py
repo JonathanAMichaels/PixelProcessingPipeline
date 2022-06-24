cd3e3ceec8edd46c2ecb2f04e3e74ac8c838d1be
JonathanAMichaels <Jonathan.asher.michaels@gmail.com>
2022-06-24 15:39:52 -0400

    myo sorting

--- sorting/myo_load.py
+++ sorting/myo_load.py
@@ -1,6 +1,8 @@
+import os
 import glob
 from pathlib import Path
 from open_ephys.analysis import Session
+import numpy as np
 
 def myo_load(config):
     directory = config['myomatrix']
@@ -14,8 +16,19 @@ def myo_load(config):
     for myomatrix in range(num_myomatrix):
         chans = range(chan_list[myomatrix][0] - 1, chan_list[myomatrix][1] - 1)
         print(chans)
-        # for each set
-        data = session.recordnodes[0].recordings[0].continuous[0].samples[:, chans]
-        sync = session.recordnodes[0].recordings[0].continuous[0].samples[:, sync_chan]
+        fs = 30000
+        ts = len(session.recordnodes[0].recordings[0].continuous[0].timestamps)
+        segs = np.round(np.linspace(0, ts, num=100, endpoint=True))
+        bin_file = directory + '/data.bin'
+        if not os.path.isfile(bin_file):
+            with open(bin_file, 'wb') as f:
+                # segment time into managable chunks
+                # for each set
+                for i in range(len(segs)-1):
+                    trange = range(segs[i], segs[i+1])
+                    data = session.recordnodes[0].recordings[0].continuous[0].samples[trange, chans]
+                    f.write(np.int16(data))
+            f.close()
+            sync = session.recordnodes[0].recordings[0].continuous[0].samples[:, sync_chan]
 
 
