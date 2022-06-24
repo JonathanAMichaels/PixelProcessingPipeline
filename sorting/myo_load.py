af90b11461954e46c417b468dc6a7d54f511e999
JonathanAMichaels <Jonathan.asher.michaels@gmail.com>
2022-06-24 16:01:09 -0400

    myo sorting

--- sorting/myo_load.py
+++ sorting/myo_load.py
@@ -3,6 +3,7 @@ import glob
 from pathlib import Path
 from open_ephys.analysis import Session
 import numpy as np
+import scipy.io.savemat as savemat
 
 def myo_load(config):
     directory = config['myomatrix']
@@ -10,15 +11,13 @@ def myo_load(config):
 
     chan_list = config['Session']['myo_chan_list']
     sync_chan = int(config['Session']['myo_analog_chan']) - 1
-    print(chan_list)
-    print(sync_chan)
     num_myomatrix = len(chan_list)
     for myomatrix in range(num_myomatrix):
         chans = range(chan_list[myomatrix][0] - 1, chan_list[myomatrix][1] - 1)
-        print(chans)
-        fs = 30000
         ts = len(session.recordnodes[0].recordings[0].continuous[0].timestamps)
         segs = np.round(np.linspace(0, ts, num=100, endpoint=True)).astype('int')
+        print(ts)
+        print(segs)
         bin_file = directory + '/data.bin'
         if not os.path.isfile(bin_file):
             with open(bin_file, 'wb') as f:
@@ -29,6 +28,7 @@ def myo_load(config):
                     data = session.recordnodes[0].recordings[0].continuous[0].samples[np.ix_(trange, chans)]
                     f.write(np.int16(data))
             f.close()
-            sync = session.recordnodes[0].recordings[0].continuous[0].samples[:, sync_chan]
-
+        sync_data = dict([])
+        sync_data['sync'] = session.recordnodes[0].recordings[0].continuous[0].samples[:, sync_chan]
+        savemat(directory + '/sync.mat', sync_data)
 
