0e619b9704e221a097a90570d1d84920c961a5a4
JonathanAMichaels <Jonathan.asher.michaels@gmail.com>
2022-06-24 14:51:25 -0400

    myo sorting

--- sorting/myo_load.py
+++ sorting/myo_load.py
@@ -1,10 +1,3 @@
-import numpy as np
-import os
-import sys
-
-script_folder = os.path.dirname(os.path.realpath(__file__))
-sys.path.append(script_folder)
-
 import glob
 from pathlib import Path
 from open_ephys.analysis import Session
