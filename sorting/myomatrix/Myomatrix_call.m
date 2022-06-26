22ebe7070198453e5b1891a6fc52709cd85c5ccf
JonathanAMichaels <Jonathan.asher.michaels@gmail.com>
2022-06-25 20:47:19 -0400

    myo resorting

--- sorting/myomatrix/Myomatrix_call.m
+++ sorting/myomatrix/Myomatrix_call.m
@@ -1,8 +1,8 @@
 load('/tmp/config.mat')
 if num_chans == 16
-    load([script_dir '../../geometries/bipolar_test_kilosortChanMap'])
+    load([script_dir '../geometries/bipolar_test_kilosortChanMap'])
 elseif num_chans == 32
-    load([script_dir '../../geometries/monopolar_test_kilosortChanMap'])
+    load([script_dir '../geometries/monopolar_test_kilosortChanMap'])
 end
 params.chanMap = cat(2, xcoords, ycoords);
 params.kiloDir = myomatrix_folder
