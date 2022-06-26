088053a8abf4ae9a139c3bc079f008cbc20a4d7c
JonathanAMichaels <Jonathan.asher.michaels@gmail.com>
2022-06-25 20:55:56 -0400

    myo resorting

--- sorting/myomatrix/Myomatrix_call.m
+++ sorting/myomatrix/Myomatrix_call.m
@@ -5,8 +5,10 @@ elseif num_chans == 32
     load([script_dir '/geometries/monopolar_test_kilosortChanMap'])
 end
 params.chanMap = cat(2, xcoords, ycoords);
-params.kiloDir = myomatrix_folder
-params.binaryFile = [myomatrix_folder '/proc.dat']
+params.kiloDir = myomatrix_folder;
+params.binaryFile = [myomatrix_folder '/proc.dat'];
 params.userSorted = true;
+params.savePlots = true;
 
 Myomatrix_resorter(params)
+quit;
