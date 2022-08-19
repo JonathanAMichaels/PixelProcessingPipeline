716ee3994b4fd47e8ddb67029ba37e35cbea23c2
Jonathan Michaels <jonathan.asher.michaels@gmail.com>
2022-08-19 11:40:26 -0400

    Adding LFP extraction

--- pipeline_utils.py
+++ pipeline_utils.py
@@ -43,12 +43,12 @@ def extract_LFP(config_kilosort):
     data = spikeglx.Reader(Path(config_kilosort['neuropixel']))
     meta = data.geometry
     print(meta)
-    sos = signal.butter(4, (0.5, 300), fs=int(data.fs), btype='bandpass', output='sos')  # 300Hz lowpass filter
+    sos = signal.butter(4, (1, 300), fs=int(data.fs), btype='bandpass', output='sos')  # 300Hz lowpass filter
     all_data = np.zeros((int(data.ns/30), data.nc), dtype=np.float32)
     print(all_data.shape)
     all = list(range(data.ns))
-    buffer_size = 30000
-    intervals = all[0: int(data.ns): 500000]
+    buffer_size = 60000
+    intervals = all[0: int(data.ns): 800000]
     print(intervals)
     I = np.zeros(2, dtype=np.int64)
     for i in range(len(intervals)):
