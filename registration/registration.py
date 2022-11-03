83f0e27be746be2c3a333d34f932f1c123396fd1
JonathanAMichaels <Jonathan.asher.michaels@gmail.com>
2022-11-02 20:16:27 -0400

    refine registration parameters

--- registration/registration.py
+++ registration/registration.py
@@ -94,8 +94,8 @@ def registration(config):
                   ' --noresidual --nowaveforms --dndetect --thresholds=12,10,8,6 --n_jobs=' + str(n_jobs) +  # 12,10,8,6
                   ' --geom=' + config['script_dir'] +
                   '/registration/spikes_localization_registration/channels_maps/np1_channel_map.npy ' +
-                  '--n_windows=3 ' +
-                  '--disp=900 --overwrite')  # 5, 1500 / 4, 1500
+                  '--n_windows=5 ' +
+                  '--disp=900 --overwrite')  # 5, 1500 / 3, 900
 
         registered_file = glob.glob(registration_directory + 'subtraction_*.h5')
         with h5py.File(registered_file[0], "r") as f:
