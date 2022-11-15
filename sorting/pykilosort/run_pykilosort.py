8eec30787fbe334a1c1a1a42540820e1905949ea
Jonathan Michaels <jonathan.asher.michaels@gmail.com>
2022-11-15 11:16:29 -0500

    trying to combine 3.0 and 2.0 again

--- sorting/pykilosort/run_pykilosort.py
+++ sorting/pykilosort/run_pykilosort.py
@@ -24,7 +24,7 @@ def kilosort(config):
         dispmap = f["dispmap"][:]
 
     params['disp_map'] = dispmap.tolist()
-    params['sig_datashift'] = 30.0
+    params['sig_datashift'] = 60.0
     run_spike_sorting_ibl(bin_file, delete=True, scratch_dir=scratch_dir, alf_path=None,
                           ks_output_dir=ks_output_dir, log_level='INFO', params=params, stop_after='drift_correction')
 
