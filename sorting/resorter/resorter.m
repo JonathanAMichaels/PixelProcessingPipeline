e690d64f827c9a916c55728ff1e7cf1730c46852
JonathanAMichaels <Jonathan.asher.michaels@gmail.com>
2022-11-19 16:43:56 -0500

    refining resorter

--- sorting/resorter/resorter.m
+++ sorting/resorter/resorter.m
@@ -374,8 +374,8 @@ nChan = size(params.chanMap,1);
 spt = recordSize*nChan;
 % Zero out channels that are bad
 if nChan <= 32
-    diag(Wrot)
-    badChan = find(diag(Wrot) < 8);
+    %diag(Wrot)
+    badChan = find(diag(Wrot) < 4);
 else
     badChan = [];
 end
