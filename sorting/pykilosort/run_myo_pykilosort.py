29284f3d9b5e91c9e71c5c5335b6ccdb90ffe2e5
JonathanAMichaels <jonathan.asher.michaels@gmail.com>
2022-12-14 15:59:54 -0500

    testing expanded template width

--- sorting/pykilosort/run_myo_pykilosort.py
+++ sorting/pykilosort/run_myo_pykilosort.py
@@ -18,7 +18,7 @@ def myo_sort(config):
     directory = config['myomatrix']
     chans = range(0, config['num_chans'])
     bin_file = directory + '/data' + str(config['myomatrix_num']) + '.bin'
-    params = {'perform_drift_registration': False, 'n_channels': len(chans), 'minfr_goodchannels': 0., 'nt0': 65} # doesn't seem to work for nt0 > 64
+    params = {'perform_drift_registration': False, 'n_channels': len(chans), 'minfr_goodchannels': 0., 'nt0': 64} # doesn't seem to work for nt0 > 64
     data_path = Path(bin_file)
     dir_path = Path(config['myomatrix_folder'])  # by default uses the same folder as the dataset
     output_dir = dir_path
