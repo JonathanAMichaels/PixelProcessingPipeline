528569059f2a88b5462f8577604f47b5f236a50b
JonathanAMichaels <Jonathan.asher.michaels@gmail.com>
2023-01-05 14:35:34 -0500

    testing

--- sorting/Kilosort-2.0/preProcess/get_whitening_matrix.m
+++ sorting/Kilosort-2.0/preProcess/get_whitening_matrix.m
@@ -45,6 +45,11 @@ while ibatch<=Nbatch
 
     ibatch = ibatch + ops.nSkipCov; % skip this many batches
 end
+
+Nbatch
+ops.nSkipCov
+CC
+
 CC = CC / ceil((Nbatch-1)/ops.nSkipCov); % normalize by number of batches
 
 fclose(fid);
