291a762443eb62898b3ad7f10b325157beaec2db
JonathanAMichaels <Jonathan.asher.michaels@gmail.com>
2023-01-05 16:57:48 -0500

    testing

--- sorting/Kilosort_config_2.m
+++ sorting/Kilosort_config_2.m
@@ -34,7 +34,7 @@ ops.reorder         = 1;       % whether to reorder batches for drift correction
 ops.nskip           = 25;  % how many batches to skip for determining spike PCs
 
 ops.GPU                 = 1; % has to be 1, no CPU version yet, sorry
-ops.Nfilt               = 128; % max number of clusters
+ops.Nfilt               = 1024; % max number of clusters
 ops.nfilt_factor        = 4; % max number of clusters per good channel (even temporary ones)
 ops.ntbuff              = 64;    % samples of symmetrical buffer for whitening and spike detection
 ops.NT                  = 64*1024+ ops.ntbuff; % must be multiple of 32 + ntbuff. This is the batch size (try decreasing if out of memory).
