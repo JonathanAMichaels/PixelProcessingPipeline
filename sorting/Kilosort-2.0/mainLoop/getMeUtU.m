d338e5c8007325bdae2b93b14bd379c115eba8ad
JonathanAMichaels <Jonathan.asher.michaels@gmail.com>
2023-01-05 21:10:07 -0500

    testing

--- sorting/Kilosort-2.0/mainLoop/getMeUtU.m
+++ sorting/Kilosort-2.0/mainLoop/getMeUtU.m
@@ -7,12 +7,12 @@ function [UtU, maskU, iList] = getMeUtU(iU, iC, mask, Nnearest, Nchan)
 
 Nfilt = numel(iU);
 
-iU
-Nfilt
+%U = gpuArray.zeros(Nchan, Nfilt, 'single'); % create a sparse matrix with ones if a channel K belongs to a template
+U = zeros(Nchan, Nfilt, 'single');
 
-U = gpuArray.zeros(Nchan, Nfilt, 'single'); % create a sparse matrix with ones if a channel K belongs to a template
-
-ix = iC(:, iU) + int32([0:Nchan:(Nchan*Nfilt-1)]); % use the template primary channel to obtain its neighboring channels from iC
+iC = gather(iC);
+ix = iC(:, iU) + int32([0:Nchan:(Nchan*Nfilt-1)]);
+%ix = iC(:, iU) + int32([0:Nchan:(Nchan*Nfilt-1)]); % use the template primary channel to obtain its neighboring channels from iC
 U(ix) = 1; % use this as an awkward index into U
 
 UtU = (U'*U) > 0; % if this is 0, the templates had not pair of channels in common
@@ -22,5 +22,6 @@ maskU = mask(:, iU); % we also return the masks for each template, picked from t
 if nargin>3 && nargout>2
     cc = UtU;
     [~, isort] = sort(cc, 1, 'descend'); % sort template pairs in order of how many channels they share
-    iList = int32(gpuArray(isort(1:Nnearest, :))); % take the Nnearest templates for each template
+    %iList = int32(gpuArray(isort(1:Nnearest, :))); % take the Nnearest templates for each template
+    iList = int32(isort(1:Nnearest, :)); % take the Nnearest templates for each template
 end
