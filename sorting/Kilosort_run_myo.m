5af20cb219f6e57f53df3c356130fd220e510ae5
JonathanAMichaels <Jonathan.asher.michaels@gmail.com>
2023-01-06 17:04:07 -0500

    testing

--- sorting/Kilosort_run_myo.m
+++ sorting/Kilosort_run_myo.m
@@ -21,11 +21,11 @@ ops.fproc   = fullfile(myomatrix_folder, 'proc.dat');
 ops.chanMap = fullfile(chanMapFile);
 ops.NchanTOT = double(num_chans);
 
-ops.nt0 = 65;
+ops.nt0 = 64;
 %ops.Th = [2 2]
 %ops.spkTh = -3;
 ops.minFR = 0.01;
-ops.NT = 4*512*1024+ ops.ntbuff;
+ops.NT = 16*64*1024+ ops.ntbuff;
 ops.nskip           = 2;  % how many batches to skip for determining spike PCs
 ops.nSkipCov            = 2; % compute whitening matrix from every N-th batch
 ops.reorder = 1;
