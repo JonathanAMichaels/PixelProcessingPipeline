3d86bd0c56ae80946e46f826878aeaa88df8a9dc
JonathanAMichaels <Jonathan.asher.michaels@gmail.com>
2023-01-07 16:07:30 -0500

    increased threshold

--- sorting/resorter/myomatrix_call.m
+++ sorting/resorter/myomatrix_call.m
@@ -10,8 +10,10 @@ params.binaryFile = [myomatrix_folder '/proc.dat'];
 params.userSorted = false;
 params.savePlots = true;
 params.waveCount = 2000;
-params.consistencyThreshold = 0.75;
-params.crit = 0.8;
+%params.consistencyThreshold = 0.75;
+params.consistencyThreshold = 0.3;
+%params.crit = 0.8;
+params.crit = 100;
 % make sure a sorting exists
 if isfile([myomatrix_folder '/spike_times.npy'])
     resorter(params)
--- sorting/resorter/resorter.m
+++ sorting/resorter/resorter.m
@@ -121,7 +121,8 @@ if ~params.skipFilter
     % Kilosort is bad at selecting which motor units are 'good', since it uses ISI as a criteria. We expect many
     % spike times to be close together.
     % Take only 'good' single units with sufficient SNR
-    C = C((SNR > params.multiSNRThreshold & spkCount > 20));
+    %C = C((SNR > params.multiSNRThreshold & spkCount > 50));
+    C = C((C_ident == 1 & spkCount > 50));
 end
 
 % Let's straight up trim off everything we don't need to save time
@@ -244,7 +245,7 @@ disp('SNR')
 SNR
 
 % Remove clusters that don't meet inclusion criteria
-saveUnits = find(SNR > params.SNRThreshold & spkCount > 20 & ...
+saveUnits = find(SNR > params.SNRThreshold & spkCount > 50 & ...
     RR >= params.consistencyThreshold);
 keepSpikes = find(ismember(I, saveUnits));
 T = T(keepSpikes);
