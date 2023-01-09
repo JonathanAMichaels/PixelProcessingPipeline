348ab74e7e09ab43ab413f79da739ea8e7a5aae0
JonathanAMichaels <Jonathan.asher.michaels@gmail.com>
2023-01-09 12:46:47 -0500

    not shifting spike times

--- sorting/resorter/myomatrix_call.m
+++ sorting/resorter/myomatrix_call.m
@@ -11,8 +11,9 @@ params.userSorted = false;
 params.savePlots = true;
 params.waveCount = 2000;
 %params.consistencyThreshold = 0.75;
-params.consistencyThreshold = 0;
+params.consistencyThreshold = -1;
 params.crit = 0.8;
+params.multiSNRThreshold = 3.0;
 % make sure a sorting exists
 if isfile([myomatrix_folder '/spike_times.npy'])
     resorter(params)
