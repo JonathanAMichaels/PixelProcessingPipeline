45be250f6a49e7ceb55df02ba788799e573cd8c6
JonathanAMichaels <Jonathan.asher.michaels@gmail.com>
2023-02-06 15:47:11 -0500

    testing

--- sorting/Kilosort_run_myo_25.m
+++ sorting/Kilosort_run_myo_25.m
@@ -27,12 +27,12 @@ ops.nt0 = 107;
 %ops.nskip           = 10;  % how many batches to skip for determining spike PCs
 %ops.nSkipCov            = 10; % compute whitening matrix from every N-th batch
 ops.sigmaMask = Inf; % we don't want a distance-dependant decay
-ops.Th = [10 4];
+ops.Th = [16 10];
 ops.nfilt_factor = 4;
 ops.nblocks = 0;
 ops.nt0min = ceil(ops.nt0/2);
-ops.nPCs = 6;
-ops.nEig = 3;
+ops.nPCs = 12;
+ops.nEig = 6;
 
 if trange(2) == 0
     ops.trange = [0 Inf];
