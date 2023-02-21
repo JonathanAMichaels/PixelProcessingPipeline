7e0fda4351842e37b14d9d6a3ec93b3588e1b812
JonathanAMichaels <Jonathan.asher.michaels@gmail.com>
2023-02-20 23:04:02 -0500

    testing

--- registration/dredge_test.py
+++ registration/dredge_test.py
@@ -43,7 +43,7 @@ geom.shape
 # the csd (which is done columnwise and then averaged across depth,
 # which is smarter.)
 
-if True:
+if False:
     ap_filter.run_preprocessing(
         raw_lfp_bin,
         ppx_lfp_bin,
@@ -64,8 +64,7 @@ lfp = np.memmap(ppx_lfp_bin, dtype=np.float32).reshape(-1, y_unique.size)
 # this took about ~15mins on my laptop (no GPU) but is much faster on GPU
 p = lfpreg.online_register_rigid(
     lfp.T,
-    disp=50,
-    adaptive_mincorr_percentile=0.1,
+    adaptive_mincorr_percentile=1,
     prior_lambda=1.0)
 #)
 
