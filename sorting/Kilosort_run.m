382d99eacc00ed1994ea1549c51d8c5d2cce783e
JonathanAMichaels <Jonathan.asher.michaels@gmail.com>
2023-03-16 20:31:53 -0400

    cleaning up

--- sorting/Kilosort_run.m
+++ sorting/Kilosort_run.m
@@ -5,61 +5,16 @@ try
     restoredefaultpath
 end
 
-addpath(genpath([script_dir '/sorting/Kilosort-3.0']))
+addpath(genpath([script_dir '/sorting/Kilosort-2.0']))
 addpath(genpath([script_dir '/sorting/npy-matlab']))
 
 chanMapFile = [script_dir '/geometries/neuropixPhase3B1_kilosortChanMap.mat'];
 disp(['Using this channel map: ' chanMapFile])
 
 phyDir = 'sorted';
-
 rootZ = [neuropixel_folder '/'];
 rootH = [rootZ phyDir '/'];
-%rootS = [rootZ phyDir '/shifted/'];
 mkdir(rootH);
-%mkdir(rootS);
-
-if trange(2) == 0
-    ops.trange = [0 Inf];
-else
-    ops.trange = trange;
-end
-
-run([script_dir '/sorting/Kilosort_config_3.m']);
-ops.fproc = fullfile(rootH, 'proc.dat');
-ops.chanMap = fullfile(chanMapFile);
-ops.nblocks = 3;
-
-ops.NchanTOT = 385; % total number of channels in your recording
-
-% find the binary file
-fs = dir(fullfile(rootZ, '*.bin'));
-ops.fbinary = fullfile(rootZ, fs(1).name);
-
-disp(['Using ' ops.fbinary])
-
-%rez = preprocessDataSub(ops);
-
-%rootReg = [neuropixel_folder '/NeuropixelsRegistration2/'];
-%reg = dir(fullfile(rootReg, 'subtraction_*'));
-%rez.ops.dispmap = h5read(fullfile(rootReg, reg(1).name), '/dispmap');
-%rez.ops.saveFolder = rootH;
-
-%rez                = datashift2_ORIGINAL(rez, 1);
-%disp('Finished datashift')
-%dshift = rez.dshift;
-%save([rootH 'displacement'], 'dshift');
-
-%chanMap = 1:length(rez.ops.chanMap);
-%xcoords = rez.xcoords;
-%ycoords = rez.ycoords;
-%Wrot = rez.Wrot;
-%save([rootS 'chanmap'], 'xcoords', 'ycoords', 'chanMap');
-%save([rootS 'Wrot'], 'Wrot')
-
-clear rez ops
-rmpath(genpath([script_dir '/sorting/Kilosort-3.0']))
-addpath(genpath([script_dir '/sorting/Kilosort-2.0']))
 
 run([script_dir '/sorting/Kilosort_config_2.m']);
 ops.fbinary = fullfile(rootH, 'proc.dat');
@@ -67,19 +22,14 @@ ops.fproc = fullfile(rootH, 'proc2.dat');
 ops.chanMap = fullfile(chanMapFile);
 ops.NchanTOT = 384; % 385
 
+disp(['Using ' ops.fbinary])
+
 if trange(2) == 0
     ops.trange = [0 Inf];
 else
     ops.trange = trange;
 end
 
-%ops.fbinary = [rootS 'shifted.dat'];
-%ops.fproc = [rootH 'proc.dat'];
-%ops.NchanTOT = 384;
-%ops.trange = [0 Inf];
-%ops.chanMap = fullfile(chanMapFile);
-%rez = rmfield(rez, {'wTEMP','wPCA','iC','dist','dshift','st0','F','F0','F0m'});
-%rez.ops = ops;
 
 % preprocess data to create temp_wh.dat
 rez = preprocessDataSub(ops);
