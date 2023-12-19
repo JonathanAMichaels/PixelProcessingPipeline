960c960d8561b0b6ae76f36b65af0b151e682323
JonathanAMichaels <Jonathan.asher.michaels@gmail.com>
2023-12-18 22:50:04 -0500

    updating registration

--- registration/registration_spikeinterface.py
+++ registration/registration_spikeinterface.py
@@ -59,11 +59,14 @@ def registration(config):
         rec1 = highpass_spatial_filter(rec1)
 
         # Step 1 : activity profile
-        peaks = detect_peaks(recording=rec1, method="locally_exclusive", detect_threshold=5, **job_kwargs)
-        np.save(motion_folder / 'peaks.npy', peaks)
+        #peaks = detect_peaks(recording=rec1, method="locally_exclusive", detect_threshold=5, **job_kwargs)
+        #np.save(motion_folder / 'peaks.npy', peaks)
         peaks = np.load(motion_folder / 'peaks.npy')
-        peaks, peak_inds = select_peaks(peaks, method='smart_sampling_amplitudes', n_peaks=5000000, **job_kwargs)
-        peak_locations = localize_peaks(recording=rec1, peaks=peaks, method="monopolar_triangulation", **job_kwargs)
+        noise_levels = si.get_noise_levels(rec1, return_scaled=False)
+        peaks, peak_inds = select_peaks(peaks, method='smart_sampling_amplitudes', n_peaks=5000000,
+                                        noise_levels=noise_levels,**job_kwargs)
+        peak_locations = localize_peaks(recording=rec1, peaks=peaks, method="monopolar_triangulation",
+                                        noise_levels=noise_levels, **job_kwargs)
         np.save(motion_folder / 'peak_locations.npy', peak_locations)
         np.save(motion_folder / 'peak_inds.npy', peak_inds)
         peak_locations = np.load(motion_folder / 'peak_locations.npy')
