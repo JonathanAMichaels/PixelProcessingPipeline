dcc8b7f422ac07a17c037b276bf50512864f0d41
JonathanAMichaels <jonathan.asher.michaels@gmail.com>
2023-12-21 14:12:00 -0500

    restructure

--- sorting/sorting_post.py
+++ sorting/sorting_post.py
@@ -3,8 +3,8 @@ from pathlib import Path
 
 def sorting_post(config):
     dataset_folder = Path(config['neuropixel_folder'])
-    sorting_folder = dataset_folder / 'kilosort2.5'
-    waveform_folder = sorting_folder / 'waveforms_kilosort2.5'
+    sorting_folder = dataset_folder / 'kilosort2.0'
+    waveform_folder = sorting_folder / 'waveforms_kilosort2.0'
     we = si.WaveformExtractor.load(waveform_folder)
     metrics = si.compute_quality_metrics(we, metric_names=['firing_rate', 'presence_ratio', 'snr', 'isi_violation',
                                                            'amplitude_median', 'amplitude_cutoff'])
